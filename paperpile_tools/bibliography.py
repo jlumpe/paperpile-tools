5e6604fee91a6c31237e2e627c557709bfc6df42
Jared Lumpe <mjlumpe@gmail.com>
2019-10-21 21:14:32 -0700

    Updates to general bibliography code

--- paperpile_tools/bibliography.py
+++ paperpile_tools/bibliography.py
@@ -1,4 +1,4 @@
-import re
+"""General Bibtex stuff."""
 
 from bibtexparser import load as load_bibtex
 from bibtexparser.bparser import BibTexParser
@@ -6,7 +6,13 @@ from bibtexparser.customization import homogenize_latex_encoding
 from bibtexparser.bibdatabase import BibDatabase
 from bibtexparser.bwriter import BibTexWriter
 
-from .util import Bijection, get_bijection
+from .util import Bijection, get_bijection, BijectionKeyConflict, KeyLoc, file_context
+
+
+__all__ = ['check_db', 'check_entries', 'make_db', 'merge_dbs',
+           'read_bibliography', 'extract_keymap', 'make_keymap',
+           'update_keys', 'revert_keys', 'write_bibliography', 'entry_diff',
+           'find_duplicate_keys']
 
 
 def check_entries(entries):
@@ -20,12 +26,31 @@ def check_entries(entries):
 
 
 def check_db(db):
-	"""Check bibtex database for issues."""
+	"""Check bibtex database for issues.
+
+	Parameters
+	----------
+	db : BibDatabase
+
+	Raises
+	------
+	KeyError
+		Duplicate key is present
+	"""
 	check_entries(db.entries)
 
 
 def make_db(entries):
-	"""Make bibtex database from list of entry dictionaries."""
+	"""Make bibtex database from list of entry dictionaries.
+
+	Parameters
+	----------
+	entries : list of dict
+
+	Returns
+	-------
+	BibDatabase
+	"""
 	entries = list(entries)
 	check_entries(entries)
 	db = BibDatabase()
@@ -34,59 +59,32 @@ def make_db(entries):
 
 
 def merge_dbs(*dbs):
-	"""Merge databases together."""
+	"""Merge databases together.
+
+	Parameters
+	----------
+	dbs : BibDatabase
+
+	Returns
+	-------
+	BibDatabase
+	"""
 	entries_dict = {}
 	for db in dbs:
 		entries_dict.update(db.entries_dict)
 	return make_db(entries_dict.values())
 
 
-def reduce_key(key):
-	"""Remove extra characters on the end of the bibtex key."""
-	if re.fullmatch(r'.*?-[A-Za-z]{2}$', key):
-		return key[:-3]
-	return key
-
-
-def assign_reduced_keys(keys, keymap=None):
-	"""Assign reduced keys, detecting and omitting duplicate assignments.
-
-	Parameters
-	----------
-	keys : iterable of str
-		List of existing keys.
-	keymap : dict or paperpile_tools.util.Bijection
-		Existing keymap.
+def default_parser():
+	"""Get a Bibtex parser with default settings.
 
 	Returns
 	-------
-	tuple
-		Tuple of ``(updates, conflicts)``. ``updates`` is a
-		:class:`paperpile_tools.util.Bijection` of ``old_keys <-> reduced_keys``
-		and ``conflicts`` is a mapping from reduced keys to lists of keys that
-		share the same reduced form.
+	BibTexParser
 	"""
-	keymap = get_bijection(keymap)
-	updates = Bijection()
-
-	revmap = {}
-	for key in keys:
-		# Skip those in existing keymap
-		if key in keymap.left:
-			continue
-		key2 = reduce_key(key)
-		if key2 != key:
-			revmap.setdefault(key2, []).append(key)
-
-	conflicts = {}
-
-	for reduced, keys in revmap.items():
-		if len(keys) == 1 and reduced not in keymap.right:
-			updates.ltr[keys[0]] = reduced
-		else:
-			conflicts[reduced] = keys
-
-	return updates, conflicts
+	parser = BibTexParser(common_strings=True)
+	# parser.customization = homogenize_latex_encoding
+	return parser
 
 
 def read_bibliography(file, check=False):
@@ -94,31 +92,33 @@ def read_bibliography(file, check=False):
 
 	Parameters
 	----------
-	file : str
+	file : str or open file object
 	check : bool
 		Check database for issues and raise exception if any are found.
 
 	Returns
 	-------
-	bibtexparser.bibdatabase.BibDatabase
+	BibDatabase
 	"""
-	parser = BibTexParser(common_strings=True)
-	parser.customization = homogenize_latex_encoding
-	db = load_bibtex(file, parser)
+	parser = default_parser()
+
+	with file_context(file, encoding='utf-8') as f:
+		db = load_bibtex(f, parser)
+
 	if check:
 		check_db(db)
+
 	return db
 
 
 def make_key_sub_comment(keymap, omitted=None):
-	"""Replace keys in bibtex database using keymap.
-
+	"""Make a comment for a bibliography file indicating key substitutions.
 
 	Parameters
 	----------
-	keymap : dict or paperpile_tools.util.Bijection
+	keymap : dict or Bijection
 	omitted : list
-		List of omitted keys
+		List of omitted keys (not present in new bibliography)
 
 	Returns
 	-------
@@ -141,43 +141,158 @@ def make_key_sub_comment(keymap, omitted=None):
 	return '\n'.join(lines) + '\n'
 
 
-def keymap_from_bibliography(db):
+def find_duplicate_keys(db, attr=None, f=None):
+	"""Find duplicate keys.
+
+	Parameters
+	----------
+	db : BibDatabase
+	attr : str
+		Entry attribute to look up. If None use the entry's Bibtex ID.
+	f : callable
+		Function with signature ``(entry, key) -> newkey`` which normalizes
+		the key before comparison.
+
+	Returns
+	-------
+	dict
+	"""
+	seen = dict()
+
+	for i, entry in enumerate(db.entries):
+		id_ = entry['ID']
+
+		try:
+			key = id_ if attr is None else entry[attr]
+		except KeyError:
+			continue
+
+		if f is not None:
+			key = f(entry, key)
+
+		value = i if attr is None else id_
+		seen.setdefault(key, []).append(value)
+
+	return {k: v for k, v in seen.items() if len(v) > 1}
+
+
+def entry_diff(e1, e2, f=None):
+	"""Find the attribute values for which two entries differ.
+
+	This is mostly intended to be used in resolving duplicates.
+
+	Parameters
+	----------
+	e1 : dict
+	e2 : dict
+	f : callable
+		Function with signature ``(entry, attrname, value)`` which normalizes
+		the values for comparison.
+
+	Returns
+	-------
+	dict
+		Mapping from attribute name to ``(e1_value, e2_value)`` pairs for
+		attributes in which the values are not equal.
+	"""
+	diff = {}
+	for key in e1.keys() & e2.keys():
+		if key == 'ID':
+			continue
+
+		v1 = e1[key]
+		v2 = e2[key]
+		if f is not None:
+			v1 = f(e1, key, v1)
+			v2 = f(e2, key, v2)
+
+		if v1 != v2:
+			diff[key] = (v1, v2)
+
+	return diff
+
+
+def extract_keymap(db, attr):
 	"""Extract key map from existing bibliography.
 
 	Parameters
 	----------
 	db : bibtexparser.bibdatabase.BibDatabase
+	attr : str
+		Attribute name to get original key from
 
 	Returns
 	-------
-	paperpile_tools.util.Bijection
+	Bijection
+		Bijection between paperpile keys and replacement keys.
+
+	Raises
+	------
+	KeyError
+		If duplicate keys are found.
 	"""
 	keymap = Bijection()
 	for key, entry in db.entries_dict.items():
 		try:
-			pp_key = entry['paperpile_key']
+			orig_key = entry[attr]
 		except KeyError:
 			continue
 
-		keymap.ltr[pp_key] = key
+		keymap.add((orig_key, key))
 
 	return keymap
 
 
-def update_keys(db, keymap, store_original=True):
-	"""Replace keys in bibtex database using keymap.
-
+def make_keymap(keys, f):
+	"""Create keymap according to function, detecting and omitting duplicate assignments.
 
 	Parameters
 	----------
-	db : bibtexparser.bibdatabase.BibDatabase
-	keymap : dict or paperpile_tools.util.Bijection
-	store_original : bool
-		Record original Paperpile key in each entry.
+	keys : iterable of str
+		List of existing keys.
+	f : callable
+		Function which takes an existing key and returns the updated key.
 
 	Returns
 	-------
-	bibtexparser.bibdatabase.BibDatabase
+	tuple
+		Tuple of ``(updates, conflicts)``. ``updates`` is a
+		:class:`Bijection` of ``old_keys <-> new_keys``
+		and ``conflicts`` is a mapping from new keys to lists of old keys that
+		map to it.
+	"""
+	updates = Bijection()
+
+	revmap = {}
+	for key in keys:
+		newkey = f(key)
+		if newkey != key:
+			revmap.setdefault(newkey, []).append(key)
+
+	conflicts = {}
+
+	for newkey, oldkeys in revmap.items():
+		if len(oldkeys) == 1:
+			updates.ltr[oldkeys[0]] = newkey
+		else:
+			conflicts[newkey] = oldkeys
+
+	return updates, conflicts
+
+
+def update_keys(db, keymap, save_attr=None):
+	"""Replace keys in bibtex database using keymap.
+
+	Parameters
+	----------
+	db : BibDatabase
+	keymap : dict or paperpile_tools.util.Bijection
+	save_attr : str
+		Attribute name to store original key for each updated entry.
+
+	Returns
+	-------
+	BibDatabase
 	"""
 	keymap = get_bijection(keymap)
 
@@ -191,29 +306,52 @@ def update_keys(db, keymap, store_original=True):
 			continue
 
 		entry['ID'] = newkey
-		if store_original and 'paperpile_key' not in entry:
-			entry['paperpile_key'] = oldkey
+		if save_attr is not None:
+			if save_attr in entry:
+				pass  # TODO
+			entry[save_attr] = oldkey
 
 	return make_db(entries)
 
 
-def revert_keys(db):
-	"""Revert keys in database to their original Paperpile values."""
+def revert_keys(db, attr, inplace=False):
+	"""Revert keys in database to their original values.
 
-	entries = list(map(dict, db.entries))
+	Parameters
+	----------
+	db : BibDatabase
+	attr : str
+		Attribute name containing the original key values.
+	inplace : bool
+		Update bibliography in place instead of returning a new one.
+
+	Returns
+	-------
+    db : BibDatabase
+	"""
+	entries = db.entries
+	if not inplace:
+		entries = list(map(dict, entries))
 
 	for entry in entries:
 		try:
-			entry['ID'] = entry.pop('paperpile_key')
+			entry['ID'] = entry.pop(attr)
 		except KeyError:
 			continue
 
-	return make_db(entries)
+	return make_db(entries) if inplace else db
 
 
 def write_bibliography(file, db):
-	"""Write bibliography entries to new file."""
+	"""Write bibliography entries to new file.
+
+	Parameters
+	----------
+	file : str or writable file object
+	db :
+	"""
 	writer = BibTexWriter()
 	writer.indent = '    '
 
-	file.write(writer.write(db))
+	with file_context(file, 'w', encoding='utf-8') as f:
+		f.write(writer.write(db))
